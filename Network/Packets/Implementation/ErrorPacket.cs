035a396a391ad0ba8a9600a2d3c06f7f438cc7f7
Adammantium <adam@devforce.de>
2022-10-20 20:36:18 +0200

    First Packet Parser stuff and Binary Packet Encoding v2

---
+++ Network/Packets/Implementation/ErrorPacket.cs
@@ -0,0 +1,8 @@
+﻿using AMP.Network.Packets.Attributes;
+
+namespace AMP.Network.Packets.Implementation {
+    [PacketDefinition((byte)PacketType.ERROR)]
+    public class ErrorPacket {
+        [SyncedVar] public string error_message;
+    }
+}
